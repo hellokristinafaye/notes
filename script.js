d985a0de24c992a45e024b1129bd94fd72d05252
Kristina Faye <hellokristinafaye@gmail.com>
2025-04-28 12:53:10 -0700

    create button functionality added

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
+
+createBtn.addEventListener("click", () => {
+    let inputBox = document.createElement("p");
+    let img = document.createElement("img");
+    inputBox.className = "input-box";
+    inputbox.setAttribute("contenteditable", "true");
+    img.srg = "images/delete.png";
+    notes.Container.appendChild(inputBox).appendChild(img);
+})
